63ada079305277016f74074cebf518e62231a256
transplanar <glen@glencooney.com>
2015-12-11 18:02:13 -0500

    Add TODO to improve email validation (maybe?)

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
+  # TODO Improve email validation?
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable,
          :confirmable
