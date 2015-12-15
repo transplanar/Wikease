a6fb3fbc3dd56a3c3ad8463eab8960d0cf5d993a
transplanar <glen@glencooney.com>
2015-12-14 19:02:57 -0500

    Correct issue with gemfile

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,3 +4,15 @@ user = User.create!(email: "admin@example.com", password: "password")
 # user.confirmed_at = Time.now
 # REVIEW this doesn't seem to be working? Had to do this again within the rails console.
 user.update_attribute(:confirmed_at, Time.now)
+
+
+20.times do
+  user = User.create!(email: Faker::Internet.email, password: Faker::Internet.password)
+  2.times do
+    # TODO make private default to false (does it do this already?)
+    user.wikis.create!(title: , body: , private: false )
+  end
+
+  # create one private wiki
+  user.wikis.create
+end
