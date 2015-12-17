2c2a49d7d5dd3961588d27e2f3e5f205401fbef6
transplanar <glen@glencooney.com>
2015-12-17 16:02:45 -0500

    Implement user role upgrade/downgrade

--- app/controllers/charges_controller.rb
+++ app/controllers/charges_controller.rb
@@ -1,39 +1,52 @@
+# include Amount
+require 'amount'
+
 class ChargesController < ApplicationController
   def new
     # Rails.configuration.stripe = {
 
     @stripe_btn_data = {
       key: "#{ Rails.configuration.stripe[:publishable_key] }",
-      description: "BigMoney Membership - #{current_user.email}",
-      # description: "Premium Membership - #{current_user.email}",
-      # TODO put this in own class/structure/block?
+      description: "Premium Membership - #{current_user.email}",
+      # REVIEW why doesn't this work?
       # amount: Amount.default
       amount: 10_00
     }
   end
 
+  def edit
+    # TODO change Stripe account to reflect downgrade
+    current_user.downgrade_role
+    flash[:notice] = "#{current_user.email} account downgraded to #{current_user.role}."
+    redirect_to user_path(current_user)
+  end
+
   def create
     # customer = Stripe::Customer.create(
-    @customer = Stripe::Customer.create(
-      email: current_user.email,
-      card: [params[:stripeToken]]
-    )
+    #   email: current_user.email,
+    #   card: [params[:stripeToken]]
+    # )
 
     # TODO create Amount class
-    charge = Stripe::Charge.create(
-      customer: customer.id,
-      amount: Amount.default,
-      # description: "Premium Membership - #{current_user.email}",
-      description: "BigMoney Membership - #{current_user.email}",
-      currency: 'usd' )
+    # charge = Stripe::Charge.create(
+    #   customer: customer.id,
+    #   amount: Amount.default,
+    #   description: "Premium Membership - #{current_user.email}",
+    #   currency: 'usd' )
 
-    flash[:notice] = "Thank you for your purchase, #{current_user.email}."
+    # flash[:notice] = "Thank you for your purchase, #{current_user.email}."
     # TODO change this to something better?
+    # XXX testing - adding user upgrade here
     redirect_to user_path(current_user)
+    current_user.upgrade_role
 
-    rescue Stripe::CardError => e
-      flash[:error] = e.message
-      redirect_to new_charge_path
+    flash[:notice] = "#{current_user.email} account upgraded to #{current_user.role}."
+
+
+    # rescue Stripe::CardError => e
+    #   flash[:error] = e.message
+    #   redirect_to new_charge_path
     end
+
   end
 # end
--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,8 @@ Rails.application.routes.draw do
   resources :users, only: [:show, :index]
   # TODO cleanup extraneous paths
   resources :wikis
-  resources :charges, only: [:new, :create]
+  # resources :charges, only: [:new, :create]
+  resources :charges, only: [:new, :create, :edit]
 
   root 'welcome#index'
 
