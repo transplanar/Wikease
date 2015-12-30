c66f3df278c9c6e99be8010efd717c1d39b75573
transplanar <glen@glencooney.com>
2015-12-30 17:27:56 -0500

    Auto-confirm seed users

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,21 +8,20 @@ user = User.create!(email: "member2@example.com", password: "password", confirme
 20.times do
   # TODO (LOW) randomize create times?
   # REVIEW (low) use seed data on heroku deployed site?
-  User.create!(email: Faker::Internet.email, password: Faker::Internet.password)
+  user = User.new(email: Faker::Internet.email, password: Faker::Internet.password)
+  user.skip_confirmation!
+  user.save!
 end
 
 User.all.each do |user|
   6.times do
     user.wikis.create!(title: Faker::Lorem.sentence , body: Faker::Lorem.paragraph)
-    user.save!
   end
 
   # user.wikis.create!(title: Faker::Lorem.sentence , body: Faker::Lorem.paragraph, private: true )
 end
 
 User.last(5).each do |user|
-  user.update_attribute(:role, 'premium')
-  # user.premium!
+  user.premium!
   user.wikis.create!(title: "PRIVATE #{Faker::Lorem.sentence}" , body: Faker::Lorem.paragraph, private: true )
-  user.save!
 end
